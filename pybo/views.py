78612190a0599690db3b58f3da89bb9ba221a728
채혜진 <>
2020-12-30 14:33:07 +0900

    2-04 질문 목록과 질문 상세 기능 구현하기

--- pybo/views.py
+++ pybo/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Question
+
 
 # Create your views here.
 
@@ -11,4 +12,13 @@ def index(request):
     question_list = Question.objects.order_by('-create_date')
     context = {'question_list': question_list}
 
-    return render(request, 'pybo/question_list.html', context)
+    return render(request, 'question_list.html', context)
+
+
+def detail(request, question_id):
+    """
+    pybo 내용 출력
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    content = {'question': question}
+    return render(request, 'question_detail.html', content)
