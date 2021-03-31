153fde109501df6b6fbf12027955077f000fea7c
채혜진 <sunnyside0907@bubblecon.io>
2021-03-31 11:35:09 +0900

    ** 진행 중지 **

--- pybo/views.py
+++ pybo/views.py
@@ -5,6 +5,7 @@ from django.utils import timezone
 from .forms import QuestionForm, AnswerForm
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 
 # Create your views here.
@@ -73,5 +74,32 @@ def question_create(request):
             return redirect('pybo:index')
     else:
         form = QuestionForm()
+    context = {'form': form}
+    return render(request, 'pybo/question_form.html', context)
+
+
+@login_required(login_url='common:login')
+def question_modify(request, question_id):
+    """
+    pybo 질문 수정
+    """
+    question = get_object_or_404(Question, pk=question_id)
+
+    # 로그인한 유저와 작성자가 다를 경우 넌필드 오류 발생
+    if request.user != question.author:
+        messages.error(request, '수정 권한이 없습니다.')
+        return redirect('pybo:detail', question_id=question.id)
+
+    if request.method == "POST":
+        # 질문 수정 화면에 기존 제목, 내용 반영
+        form = QuestionForm(request.POST, instance=question)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.author = request.user
+            question.save()             # 수정일시 저장
+
+    else:
+        form = QuestionForm(instance=question)
+
     context = {'form': form}
     return render(request, 'pybo/question_form.html', context)
